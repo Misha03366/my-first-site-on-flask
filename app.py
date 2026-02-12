89692d48e59e69540d093867c983cd1eb2376cfc
Misha03366 <mishaivlev48166@gmail.com>
2026-02-12 22:15:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Работает!</h1><p>Я этого рот ебал</p>"
+    return render_template('hello.html')
 
 
 if __name__ == "__main__":
     app.run()
+
 
 
