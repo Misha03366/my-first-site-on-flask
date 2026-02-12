350de09955bbee4ed1899f4e896f96ec51da8547
Misha03366 <mishaivlev48166@gmail.com>
2026-02-12 21:09:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Работает!</h1><p>Сайт запущен</p>"
+    return "<h1>Работает!</h1><p>Я этого рот ебал</p>"
 
 
 if __name__ == "__main__":
     app.run()
+
 
